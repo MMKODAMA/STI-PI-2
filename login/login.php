0f370c2176bf9d404a9c368a251a3615e81ab253
Makoto <matheus.mkodama@gmail.com>
2021-12-02 22:08:31 -0300

    update

--- login/login.php
+++ login/login.php
@@ -3,12 +3,9 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-session_start();
+require_once('../connection/connectionFactory.php');
 
-
-require_once('connection/connectionFactory.php');
-
-$stmt = $db->prepare('SELECT id,email,senha FROM usuarios WHERE email = :email');
+$stmt = $db->prepare('SELECT id,email,senha FROM usuario WHERE email = :email');
 
 $stmt->execute([':email'=> $_POST['email']]);
 
@@ -16,20 +13,12 @@ $registro = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if($registro){
     if(password_verify($_POST['password'], $registro['senha'])){
-        
-        $_SESSION['id'] = $registro['id'];
-        header('Location: cadastro/cadastro.html');
+        header('Location: ../home/index.php');
     }else{
-        
-        session_destroy();
+
         echo 'Credenciais invalidas<br><br><a href="login.html">Tente Novamente</a>';
     }
 }else{
-    session_destroy();
+
     echo 'Credenciais invalidas<br><br><a href="login.html">Tente Novamente</a>';
 }
-
-if(!isset($_SESSION['id'])){
-    echo 'Acesso Negado';
-    exit();
-} 

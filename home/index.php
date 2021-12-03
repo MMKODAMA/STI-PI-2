56cc02792dbc6d861e3290df457ea4f5510dc81a
Leticia Higino <leticialima.hn@gmail.com>
2021-12-02 23:51:24 -0300

    final

--- home/index.php
+++ home/index.php
@@ -60,7 +60,7 @@ include('../appProduto/telas/header.php');
                     <div class="modal-dialog">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" style="color:black" id="exampleModalLabel">Descrição</h5>
+                            <h5 class="modal-title" style="color:black"  style="color:black" id="exampleModalLabel">Descrição</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body" style="color:black">
